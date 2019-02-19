7fb082a80f08901cc377683c4e9fea7562f2f0da
u296 <42377419+u296@users.noreply.github.com>
2019-02-19 22:24:26 +0000

    lel wtf am i doing

--- commandlineFps/commandlineFps.cpp
+++ commandlineFps/commandlineFps.cpp
@@ -1,21 +1,157 @@
-// commandlineFps.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include "pch.h"
 #include <iostream>
+#include <string>
+#include <cmath>
+#include <Windows.h>
+
+
+namespace screen
+{
+	const int width = 960;
+	const int height = 540;
+	const int pixelCount = width * height;
+
+	wchar_t * windowContents = new wchar_t[width * height];
+	HANDLE console = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	DWORD dwordBytesWritten = 0;
+}
+
+namespace game
+{
+	namespace logic
+	{
+		bool running = true;
+	}
+
+	class Player
+	{
+	public:
+		Player(double xCoordinate, double yCoordinate, double zCoordinate, double zRotation, double fieldOfView, double maxViewDistance)
+		{
+			this->xCoordinate = xCoordinate;
+			this->yCoordinate = yCoordinate;
+			this->zCoordinate = zCoordinate;
+
+			this->zRotation = zRotation;
+
+			this->fieldOfView = fieldOfView;
+			this->maxViewDistance = maxViewDistance;
+		}
+		Player()
+		{
+			xCoordinate = 0;
+			yCoordinate = 0;
+			zCoordinate = 0;
+
+			zRotation = 0;
+
+			fieldOfView = 90;
+			maxViewDistance = 0;
+		}
+		~Player()
+		{
+
+		}
+		double xCoordinate;
+		double yCoordinate;
+		double zCoordinate;
+
+		double zRotation;
+
+		double fieldOfView;
+		double maxViewDistance;
+	};
+
+	class Map
+	{
+	public:
+		Map(int height, int width, std::wstring representation)
+		{
+			this->height = height;
+			this->width = width;
+			this->representation = representation;
+		}
+		~Map()
+		{
+
+		}
+
+		int height;
+		int width;
+
+		std::wstring representation;
+
+	};
+}
+
+void WriteToConsole()
+{
+	screen::windowContents[screen::width * screen::height - 1] = '\0';
+	WriteConsoleOutputCharacter(screen::console, screen::windowContents, screen::pixelCount, { 0,0 }, &screen::dwordBytesWritten);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+
+	SetConsoleActiveScreenBuffer(screen::console);
+
+	
+	
+	std::wstring mapData;
+
+	mapData += L"############";
+	mapData += L"#          #";
+	mapData += L"#          #";
+	mapData += L"#          #";
+	mapData += L"#          #";
+	mapData += L"#          #";
+	mapData += L"#          #";
+	mapData += L"#          #";
+	mapData += L"#          #";
+	mapData += L"#          #";
+	mapData += L"#          #";
+	mapData += L"############";
+
+	game::Map map = game::Map(12, 12, mapData);
+
+	game::Player player = game::Player(0.0, 0.0, 0.0, 0.0, 90, 90);
+
+	while (game::logic::running)
+	{
+		// Render frame
+
+		for (int pixelCollumn = 0; pixelCollumn < screen::width; pixelCollumn++)
+		{
+			double rayAngle = (player.zRotation - player.fieldOfView / 2) + ((double) pixelCollumn / (double) screen::width) * player.fieldOfView;
+
+			int distanceToWall;
+
+			bool rayHitWall = false;
+
+			double xTestIncrement = std::sin(rayAngle);
+			double yTestIncrement = std::cos(rayAngle);
+
+			while (!rayHitWall || distanceToWall > player.maxViewDistance)
+			{
+				distanceToWall++;
+
+				int testX = player.xCoordinate + distanceToWall * xTestIncrement;
+				int testY = player.xCoordinate + distanceToWall * xTestIncrement;
+
+				if (map.representation[(map.width * testY) + testX] == '#')
+				{
+
+				}
+			}
+
+		}
+
+		// Push frame to console
+		WriteToConsole();
+	}
+
+	programEnd:
+		std::cout << "program end. press return to exit" << std::endl;
+		std::string exitInput;
+		std::getline(std::cin, exitInput);
+		return EXIT_SUCCESS;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
